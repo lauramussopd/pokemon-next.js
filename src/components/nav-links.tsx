a45c27e02ff5f9417b7006eaa99dfe3ee87d8f5d
Laura Musso <lauramussopd@gmail.com>
2024-09-05 14:14:14 +0200

    working on tfeature

--- src/components/nav-links.tsx
+++ src/components/nav-links.tsx
@@ -1,40 +1,40 @@
 "use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
-const NavLinks = ({ pokemons }: { pokemons: { name: string; imageUrl: string }[] }) => {
+const NavLinks = ({
+  pokemons,
+}: {
+  pokemons: { name: string; imageUrl: string }[];
+}) => {
   const pathname = usePathname();
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-8">
       {pokemons.map((pokemon) => (
         <Link
           key={pokemon.name}
           href={`/details/${pokemon.name.toLowerCase()}`}
-          className={`flex items-center gap-4 p-2 rounded-md transition-transform duration-300 ${
+          className={`flex  justify-center pr-20 card text-green-900 items-center ${
             pathname === `/details/${pokemon.name.toLowerCase()}`
-              ? 'bg-green-600 text-white hover:bg-green-700'
-              : 'bg-white text-green-900 hover:bg-gray-100'
           }`}
         >
-          <div
-            className={`transition-transform duration-300 ${
+          {/* <div
+            className={`${
               pathname === `/details/${pokemon.name.toLowerCase()}`
-                ? 'hover:scale-120'
-                : 'hover:scale-140'
             }`}
-          >
-          </div>
-          <span className="text-center items-center flex-1">
-          <Image
+          ></div> */}
+          {/* <span className="text-center items-center flex-1"> */}
+            <Image
               src={pokemon.imageUrl}
               alt={pokemon.name}
-              width={100}
-              height={100}
-              className="rounded-full"
-            />{pokemon.name}</span>
+              width={130}
+              height={130}
+            />
+            <p className="pokemon-name text-center">{pokemon.name}</p>
+          {/* </span> */}
         </Link>
       ))}
     </div>
